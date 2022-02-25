1146278239d2202022cf6cefa10ea4886c2b7c0b
tiisachenko <tiisachenko@dasreda.ru>
2022-02-25 20:50:29 +0300

    UPDATE added new class

--- src/com/timurisachenko/Main.java
+++ src/com/timurisachenko/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("HELLO YLAB welcomes you!");
+        System.out.println("Are you sure this is constanta?" + Util.CONSTANTA);
     }
 }
---
+++ src/com/timurisachenko/Util.java
@@ -0,0 +1,5 @@
+package com.timurisachenko;
+
+public class Util {
+    public static final String CONSTANTA = "CONSTANTA";
+}
